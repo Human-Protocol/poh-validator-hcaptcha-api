a762c6305c6c5ac39bd112b26568992900299fc3
Alex Bakoushin <alex@bakoush.in>
2022-04-30 11:22:25 +0700

    Update env variable naming

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const port = process.env.PORT;
 const validatorKey = process.env.VALIDATOR_KEY;
-const hcaptchaToken = process.env.HCAPTCHA_TOKEN;
+const hcaptchaSitekey = process.env.HCAPTCHA_SITEKEY;
 const hcaptchaSecret = process.env.HCAPTCHA_SECRET;
 
-export { port, validatorKey, hcaptchaToken, hcaptchaSecret };
+export { port, validatorKey, hcaptchaSitekey, hcaptchaSecret };

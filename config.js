ea1018af70b4e8b13a00ee35c20925a44bf2e696
Alex Bakoushin <alex@bakoush.in>
2022-07-24 10:29:55 +0700

    Add env variables check throwing an error. Fix #1

--- config.js
+++ config.js
@@ -2,4 +2,23 @@ const port = process.env.PORT;
 const validatorKey = process.env.VALIDATOR_KEY;
 const hcaptchaSecret = process.env.HCAPTCHA_SECRET;
 
+let errors = [];
+
+if (!port) {
+  errors.push("Error: PORT environment variable is not set.");
+}
+if (!validatorKey) {
+  errors.push("Error: VALIDATOR_KEY environment variable is not set.");
+}
+if (!hcaptchaSecret) {
+  errors.push("Error: HCAPTCHA_SECRET environment variable is not set.");
+}
+
+if (errors.length) {
+  for (const error of errors) {
+    console.error(error);
+  }
+  throw new Error("Insufficient environment varables");
+}
+
 export { port, validatorKey, hcaptchaSecret };
